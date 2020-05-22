f120af7515a74666b423e3ec867820173421a5bf
shanshenshi <shanshenshi@126.com>
2020-05-22 21:02:46 +0800

    support CMAKE_USE_OLD_GDB

--- build_rocksdb.sh
+++ build_rocksdb.sh
@@ -7,9 +7,9 @@ rocksdb_home=${work_home}
 
 deps_prefix=${rocksdb_home}/rocksdb_bin
 cmake -H${rocksdb_home} -Bcmake-build -DCMAKE_INSTALL_PREFIX:PATH=${deps_prefix} \
-      -DCMAKE_PREFIX_PATH=${deps_prefix} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Release} \
+      -DCMAKE_PREFIX_PATH=${deps_prefix} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Debug} \
       -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" -DWITH_TOOLS=OFF -DCMAKE_EXE_LINKER_FLAGS=-lrt \
-      -DCMAKE_SHARED_LINKER_FLAGS=-lrt -DUSE_RTTI=1
+      -DCMAKE_SHARED_LINKER_FLAGS=-lrt -DUSE_RTTI=1 -DWITH_TESTS=OFF -DCMAKE_USE_OLD_GDB=1
 
 cmake --build cmake-build -- -j4
 cmake --build cmake-build -- all install
